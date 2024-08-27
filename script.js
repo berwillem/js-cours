cf84d5851ed3fbddeae2dc9a41d3a378d1c20d2f
berchiche willem <berwillempro@gmail.com>
2024-08-27 14:59:05 +0100

    dark mode

---
+++ script.js
@@ -0,0 +1,10 @@
+var titre = document.querySelector("h1");
+var body = document.querySelector("body");
+function dark() {
+  titre.style.color = "white";
+  body.style.backgroundColor = "black";
+}
+function light() {
+  titre.style.color = "black";
+  body.style.backgroundColor = "white";
+}
